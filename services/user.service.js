53b812fde8bcf8af75034b233757ea48ea3eec08
paulov <paulos_50@hotmail.com>
2024-04-16 00:07:59 -0500

    DB users

--- services/user.service.js
+++ services/user.service.js
@@ -3,41 +3,54 @@
 // eslint-disable-next-line no-unused-vars
 const boom = require('@hapi/boom');
 //const getConnection = require('../lib/postgres');
-const pool = require('../lib/postgres.pool');
+//const pool = require('../lib/postgres.pool');
+
+//sequelize con db
+const  { models } = require('./../lib/sequelize');
+
 
 class UserService {
   constructor() {
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
+    //this.pool = pool;
+    //this.pool.on('error', (err) => console.error(err));
 
   }
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
     //return [];
-    //const client = await getConnection();                      //conexión con la bdd lib postgres
+    //const client = await getConnection();                      //conexión con la DB lib postgres
     //const rta = await client.query('SELECT * FROM task');
 
-    const query = 'SELECT * FROM task';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    //const query = 'SELECT * FROM task';                        //conexión con la DB pool
+    //const rta = await this.pool.query(query);
+    //return rta.rows;
+
+    const rta =  await models.User.findAll();
+    return rta;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user){
+      throw boom.notFound('usuario no encontrado');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy(id);
     return { id };
   }
 }
